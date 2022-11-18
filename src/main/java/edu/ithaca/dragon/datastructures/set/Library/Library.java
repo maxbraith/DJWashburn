831fdb23ba109613c13a3f909cf71d2d6acc5579
ghoman <griffhoho@gmail.com>
2022-11-18 11:38:30 -0500

    Library Interface Implemented

--- src/main/java/edu/ithaca/dragon/datastructures/set/Library/Library.java
+++ src/main/java/edu/ithaca/dragon/datastructures/set/Library/Library.java
@@ -1,4 +1,38 @@
 package edu.ithaca.dragon.datastructures.set.Library;
 
+import edu.ithaca.dragon.datastructures.set.CollectionOfPlaylists.CollectionOfPlaylists;
+import edu.ithaca.dragon.datastructures.set.Song.Song;
+
+import java.util.List;
+
 public interface Library {
+    /**
+     * @return a list of songs in alphabetical order contained within the library (artist first)
+     */
+    String songs();
+
+    /**
+     * @param song - a string of a valid song title
+     * @param artist - a string of a valid artist
+     * @post find a specific song's information
+     * @return the information found
+     */
+    Song songSearch(String song, String artist);
+
+    /**
+     * @param listOfSongs - a list of songs
+     * @post add this list of songs to the library
+     */
+    void addSongs(List listOfSongs);
+
+    /**
+     * @param listOfSongs - a list of songs
+     * @post remove this list of songs from the library AND any playlist in which they occur
+     */
+    void removeSongs(List listOfSongs);
+
+    /**
+     * @return the collection of playlists within the library
+     */
+    CollectionOfPlaylists getPlaylists();
 }
