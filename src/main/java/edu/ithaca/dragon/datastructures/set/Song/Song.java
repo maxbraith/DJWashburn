1be18de57888d8cb8afacdcb392d144d72c468f5
Giovanni <giovannicioffi6180@gmail.com>
2022-11-18 11:51:52 -0500

    committed half of the song interface

--- src/main/java/edu/ithaca/dragon/datastructures/set/Song/Song.java
+++ src/main/java/edu/ithaca/dragon/datastructures/set/Song/Song.java
@@ -1,4 +1,14 @@
 package edu.ithaca.dragon.datastructures.set.Song;
 
 public interface Song {
+
+    public String getArtist();
+
+    public String getSongTitle();
+
+    public int durationSeconds();
+
+    public int getNumTimesPlayed();
+
+
 }
