89cfcf6284c6672638ad178a904409c2ac2b7c56
Giovanni <giovannicioffi6180@gmail.com>
2022-11-23 13:23:39 -0500

    merged collection of playlists

--- src/main/java/edu/ithaca/dragon/datastructures/set/CollectionOfPlaylists/CollectionOfPlaylists.java
+++ src/main/java/edu/ithaca/dragon/datastructures/set/CollectionOfPlaylists/CollectionOfPlaylists.java
@@ -1,4 +1,48 @@
 package edu.ithaca.dragon.datastructures.set.CollectionOfPlaylists;
 
+import edu.ithaca.dragon.datastructures.set.Library.Library;
+import edu.ithaca.dragon.datastructures.set.Song.Song;
+
 public interface CollectionOfPlaylists {
+    /**
+     * @param song - valid name of song
+     * @param artist - valid name of artist
+     * @post remove the specific song from all playlists
+     * @throws NotFoundException if the item does not exist
+     */
+    public void removeSongFromAllPlaylists(Song songToRemove);
+    /**
+     * @param playlistName - valid name for a playlist
+     * @param seconds - length of time for the playlist (no more no less)
+     * @post create a playlist with the playlist name
+     * @post randomly add songs to the playlist to make it the exact time specified by the seconds
+     * @throws InstanceAlreadyExistsException if playlist name is already used
+     * @throws IllegalArgumentException if seconds is less than 10
+     */
+    public void createPlaylist(int durationSeconds, String name, Library library);
+    /**
+     * @param playlistName - valid name for a playlist
+     * @post create an empty playlist using the playlistName
+     * @throws InstanceAlreadyExistsException if playlist name is already used
+     */
+    public void createEmptyPlaylist(String name);
+    /**
+     * @param playlist - valid name of a playlist within the collection
+     * @post find the playlist within the collection
+     * @post if the playlist exists, delete it from the library
+     * @throws NotFoundException if the item does not exist
+     */
+    public void removePlaylist(String name);
+     /**
+     *
+     * @param playlist - valid name of a playlist within the collection
+     * @post find the playlist within the collection
+     * @return null if playlist does not exist
+     * @return the playlist object
+     */
+    public String returnSongsInPlaylist(String name);
+    /**
+     * @return a string of all playlist names and their durations within the collection
+     */
+    public String returnCollectionOfPlaylistsInfo();
 }
