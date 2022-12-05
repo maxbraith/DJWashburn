31d60cf601e758e657a74ae67bf7ed5485ef63d7
ghoman <griffhoho@gmail.com>
2022-12-05 11:12:20 -0500

    Created the efficiency test

---
+++ src/test/java/edu/ithaca/dragon/datastructures/set/EfficiencyTest/LibraryEffeciencyTest.java
@@ -0,0 +1,19 @@
+package edu.ithaca.dragon.datastructures.set.EfficiencyTest;
+
+import edu.ithaca.dragon.datastructures.set.Library.Library;
+import edu.ithaca.dragon.datastructures.set.Library.LibraryList;
+import org.junit.jupiter.api.Test;
+
+public class LibraryEffeciencyTest {
+
+    @Test
+    public void libraryEffeciency() {
+        Library libraryList = new LibraryList();
+    }
+
+    public void allTests(Library test) {
+
+    }
+
+    public void
+}
