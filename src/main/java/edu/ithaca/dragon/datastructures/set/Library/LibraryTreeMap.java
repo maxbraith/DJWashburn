2ec7fd00ec3d60c063ea0d8ae1b0a1cdc11d38b2
Max Braithwaite <mbraithwaite@ithaca.edu>
2022-12-09 12:05:55 -0500

    added comments

--- src/main/java/edu/ithaca/dragon/datastructures/set/Library/LibraryTreeMap.java
+++ src/main/java/edu/ithaca/dragon/datastructures/set/Library/LibraryTreeMap.java
@@ -11,32 +11,44 @@ public class LibraryTreeMap implements Library {
   public LibraryTreeMap(){
       songMap = new TreeMap<String, Song>();
   }
+  
+  //Returns a string of all the songs in alphabetical order
   @Override
   public String songs() {
       String songString = "";
+      //Iterates through tree map and builds a string of artists and songs
       for(String key : songMap.keySet()){
           songString += songMap.get(key).getArtist() + ": " +songMap.get(key).getSongTitle() + "\n";
       }
       return songString;
   }
+
+  //Returns a song object from treemap using the ID of the song
   public Song getSongFromID(String songID) {
+    //Checks that a song with that key exists
     if(contains2(songID) == true){
+        //returns song using ID if it does exist
       return songMap.get(songID);
     }
     else{
+        //throws IndexOutOfBoundsException if song does not exist
         throw new IndexOutOfBoundsException();
     }
   }
   @Override
+
+  //Iterates through array list adn adds songs from array list to tree map
   public void addSongs(ArrayList<Song> listOfSongs) {
       for(int i=0; i<listOfSongs.size(); i++){
           songMap.put(listOfSongs.get(i).getId(), listOfSongs.get(i));
       }
     
   }
+  //Adds a song object to tree map
   public void addSong(Song song){
     songMap.put(song.getId(), song);
   }
+  //Iterates through array list and removes instances from tree map
   @Override
   public void removeSongs(ArrayList<Song> listOfSongs) {
       for(int i=0; i<listOfSongs.size(); i++){
@@ -44,33 +56,40 @@ public class LibraryTreeMap implements Library {
       }
     
   }
+  //Returns size of treemap
   @Override
   public int getListSize() {
       return songMap.size();
   }
- 
+  
   @Override
   public Song songSearch(String songTitle, String artist) {
+        //Creats key and uses that key to check if song exists
        if(contains2(artist + ", "+ songTitle)==true){
-            if ((songMap.get(artist + ", " + songTitle).getArtist().equalsIgnoreCase(artist))&&(songMap.get(artist + ", " + songTitle).getSongTitle().equalsIgnoreCase(songTitle))){
-                return getSongFromID(artist + ", " + songTitle);
-            }
+            //finds and returns song if it does exist
+            return getSongFromID(artist + ", " + songTitle);
+            
         }
+        //returns null if it does not
        return null;
+    
   }
   @Override
   public Song shortestSongDuration() {
       if (songMap.size()==0 || songMap==null){
           return null;
       }
+      //Sets shortest song and shortest duration to that of the first item in tremap
       int shortestDuration = songMap.get(songMap.firstKey()).getDurationSeconds();
       Song shortestSong = songMap.get(songMap.firstKey());
+      //Iterates through map and compares song lenghts to find shortest song
       for(String key : songMap.keySet()){
           if(songMap.get(key).getDurationSeconds() < shortestDuration){
               shortestDuration = songMap.get(key).getDurationSeconds();
               shortestSong = songMap.get(key);
           }
       }
+      //returns shortest song
       return shortestSong;
   }
 
@@ -82,22 +101,18 @@ public class LibraryTreeMap implements Library {
   }
 @Override
 public String getArtist(Song song) {
-    // TODO Auto-generated method stub
     return song.getArtist();
 }
 @Override
 public String getSongTitle(Song song) {
-    // TODO Auto-generated method stub
     return song.getSongTitle();
 }
 @Override
 public int getDurationSeconds(Song song) {
-    // TODO Auto-generated method stub
     return song.getDurationSeconds();
 }
 @Override
 public int getNumTimesPlayed(Song song) {
-    // TODO Auto-generated method stub
     return song.getNumTimesPlayed();
 }
 @Override
